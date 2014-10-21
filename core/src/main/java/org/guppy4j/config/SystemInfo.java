ab021ff7105fd5edb5fad2f0f5d13dc1f6244ce2
odoepner <odoepner@gmail.com>
2014-10-20 22:00:04 -0300

    Fix javadoc format

--- core/src/main/java/org/guppy4j/config/SystemInfo.java
+++ core/src/main/java/org/guppy4j/config/SystemInfo.java
@@ -13,20 +13,20 @@ public interface SystemInfo {
 
     /**
      * @return File path component separator, e.g. '\' on Windows, '/' on Unix
-     * @see 'file.separator' system property
+     * @see "The 'file.separator' 'system property""
      */
     char fileSeparator();
 
     /**
      * @return Separator for jar and directory paths in classpath and system path,
      * e.g. ';' on Windows, ':' on Unix
-     * @see 'path.separator' system property
+     * @see "The 'path.separator' 'system property"
      */
     char pathSeparator();
 
     /**
      * @return Character sequence that separates lines in text files
-     * @see 'line.separator' system property
+     * @see "The 'line.separator' 'system property"
      */
     String lineSeparator();
 
@@ -34,68 +34,68 @@ public interface SystemInfo {
      * @return Ordered list of directories and JAR files, see
      * <a href='http://docs.oracle.com/javase/8/docs/technotes/tools/windows/classpath.html'>
      * Classpath documentation</a> for details
-     * @see 'java.class.path' system property
+     * @see "The 'java.class.path' 'system property"
      */
     Iterable<Path> javaClassPath();
 
     /**
      * @return Installation directory for Java Runtime Environment (JRE),
-     * @see 'java.home' system property
+     * @see "The 'java.home' 'system property"
      */
     Path javaHome();
 
     /**
      * @return Current working directory of this Java process
-     * @see 'user.dir' system property
+     * @see "The 'user.dir' 'system property"
      */
     Path userDir();
 
     /**
      * @return User home directory
-     * @see 'user.home' system property
-     * @see <a href='http://bugs.java.com/view_bug.do?bug_id=4787931'>JDK-4787931</a> bug
+     * @see "The 'user.home' 'system property""
+     * @see "http://bugs.java.com/view_bug.do?bug_id=4787931"
      */
     Path userHome();
 
     /**
      * @return JRE vendor name
-     * @see 'java.vendor' system property
+     * @see "The 'java.vendor' 'system property"
      */
     String javaVendor();
 
     /**
      * @return JRE vendor URL
-     * @see 'java.vendor.url' system property
+     * @see "The 'java.vendor.url' 'system property"
      */
     URL javaVendorUrl();
 
     /**
      * @return JRE version number
-     * @see 'java.version' system property
+     * @see "The 'java.version' 'system property"
      */
     String javaVersion();
 
     /**
      * @return Operating system architecture
-     * @see 'os.arch' system property
+     * @see "The 'os.arch' 'system property"
      */
     String osArch();
 
     /**
      * @return Operating system name
-     * @see 'os.name' system property
+     * @see "The 'os.name' 'system property"
      */
     String osName();
 
     /**
      * @return Operating system version
-     * @see 'os.version' system property
+     * @see "The 'os.version' 'system property"
      */
     String osVersion();
 
     /**
      * @return User account name
-     * @see 'user.name' system property
+     * @see "The 'user.name' 'system property"
      */
     String userName();
 }
