93b8d1786d3dcc03fe49ef2e7e8eeb1679267f39
odoepner <odoepner@gmail.com>
2014-10-20 21:33:54 -0300

    Fix javadoc

--- core/src/main/java/org/guppy4j/config/SystemInfo.java
+++ core/src/main/java/org/guppy4j/config/SystemInfo.java
@@ -5,7 +5,7 @@ import java.nio.file.Path;
 
 /**
  * Type-safe access to standard
- * <a href="http://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html">
+ * <a href='http://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html'>
  * System properties</a>
  */
 @SuppressWarnings("UnusedDeclaration")
@@ -13,89 +13,89 @@ public interface SystemInfo {
 
     /**
      * @return File path component separator, e.g. '\' on Windows, '/' on Unix
-     * @see "file.separator" system property
+     * @see 'file.separator' system property
      */
     char fileSeparator();
 
     /**
      * @return Separator for jar and directory paths in classpath and system path,
      * e.g. ';' on Windows, ':' on Unix
-     * @see "path.separator" system property
+     * @see 'path.separator' system property
      */
     char pathSeparator();
 
     /**
      * @return Character sequence that separates lines in text files
-     * @see "line.separator" system property
+     * @see 'line.separator' system property
      */
     String lineSeparator();
 
     /**
      * @return Ordered list of directories and JAR files, see
-     * <a href="http://docs.oracle.com/javase/8/docs/technotes/tools/windows/classpath.html">
+     * <a href='http://docs.oracle.com/javase/8/docs/technotes/tools/windows/classpath.html'>
      * Classpath documentation</a> for details
-     * @see "java.class.path" system property
+     * @see 'java.class.path' system property
      */
     Iterable<Path> javaClassPath();
 
     /**
      * @return Installation directory for Java Runtime Environment (JRE),
-     * @see "java.home" system property
+     * @see 'java.home' system property
      */
     Path javaHome();
 
     /**
      * @return Current working directory of this Java process
-     * @see "user.dir" system property
+     * @see 'user.dir' system property
      */
     Path userDir();
 
     /**
      * @return User home directory
-     * @see "user.home" system property
-     * @see <a href="http://bugs.java.com/view_bug.do?bug_id=4787931">JDK-4787931</a> bug
+     * @see 'user.home' system property
+     * @see <a href='http://bugs.java.com/view_bug.do?bug_id=4787931'>JDK-4787931</a> bug
      */
     Path userHome();
 
     /**
      * @return JRE vendor name
-     * @see "java.vendor" system property
+     * @see 'java.vendor' system property
      */
     String javaVendor();
 
     /**
      * @return JRE vendor URL
-     * @see "java.vendor.url" system property
+     * @see 'java.vendor.url' system property
      */
     URL javaVendorUrl();
 
     /**
      * @return JRE version number
-     * @see "java.version" system property
+     * @see 'java.version' system property
      */
     String javaVersion();
 
     /**
      * @return Operating system architecture
-     * @see "os.arch" system property
+     * @see 'os.arch' system property
      */
     String osArch();
 
     /**
      * @return Operating system name
-     * @see "os.name" system property
+     * @see 'os.name' system property
      */
     String osName();
 
     /**
      * @return Operating system version
-     * @see "os.version" system property
+     * @see 'os.version' system property
      */
     String osVersion();
 
     /**
      * @return User account name
-     * @see "user.name" system property
+     * @see 'user.name' system property
      */
     String userName();
 }
