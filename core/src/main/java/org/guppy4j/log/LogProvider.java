1bff040211866bf094d941063ad0ec0dc68fed44
odoepner <odoepner@gmail.com>
2015-01-16 20:10:56 -0400

    Add NoOp instances

--- core/src/main/java/org/guppy4j/log/LogProvider.java
+++ core/src/main/java/org/guppy4j/log/LogProvider.java
@@ -5,6 +5,19 @@ package org.guppy4j.log;
  */
 public interface LogProvider {
 
+    /**
+     * @param c A class
+     * @return A logger instance for the class
+     */
     Log getLog(Class<?> c);
 
+    /**
+     * A LogProvider instance that does nothing
+     */
+    LogProvider NONE = new LogProvider() {
+        @Override
+        public Log getLog(Class<?> c) {
+            return Log.NONE;
+        }
+    };
 }
