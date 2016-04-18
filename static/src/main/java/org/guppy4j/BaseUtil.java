f621fed532ab390a6b5afc9516d0bbd6f66a2d98
odoepner <oliver@doepner.net>
2016-04-18 10:05:27 -0300

    Helper methods for conditional casting, useful for equals() implementations

--- static/src/main/java/org/guppy4j/BaseUtil.java
+++ static/src/main/java/org/guppy4j/BaseUtil.java
@@ -28,4 +28,28 @@ public class BaseUtil {
     public static boolean not(boolean b) {
         return !b;
     }
+
+    /**
+     * Conditional casting, useful for typical equals() implementations
+     *
+     * @param o   The other object
+     * @param c   The class to check for
+     * @param <T> The generic type of the target class
+     * @return The object cast as a T, or null if o.getClass() is not c
+     */
+    public static <T> T castIfSameClass(Object o, Class<T> c) {
+        return o != null && o.getClass() == c ? c.cast(o) : null;
+    }
+
+    /**
+     * Conditional casting, useful for equals() implementations using instanceof
+     *
+     * @param o   The other object
+     * @param c   The class to check for
+     * @param <T> The generic type of the target class
+     * @return The object cast as a T, or null if o is not an instance of c
+     */
+    public static <T> T castIfInstance(Object o, Class<T> c) {
+        return o != null && c.isInstance(o) ? c.cast(o) : null;
+    }
 }
