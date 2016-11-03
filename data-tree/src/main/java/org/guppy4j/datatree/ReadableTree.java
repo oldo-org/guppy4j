e27abd2ef64cbc537541655eba61beddfa61f613
odoepner <oliver@doepner.net>
2016-11-03 09:52:39 -0300

    Get rid of "Payload" name

--- data-tree/src/main/java/org/guppy4j/datatree/ReadableTree.java
+++ data-tree/src/main/java/org/guppy4j/datatree/ReadableTree.java
@@ -3,7 +3,7 @@ package org.guppy4j.datatree;
 /**
  * Readable tree structure
  */
-public interface ReadableTree extends ReadablePayload {
+public interface ReadableTree extends ReadableData {
 
     Iterable<? extends ReadableTree> nodes(String expression);
 }
--- data-tree/src/main/java/org/guppy4j/datatree/Tree.java
+++ data-tree/src/main/java/org/guppy4j/datatree/Tree.java
@@ -3,7 +3,7 @@ package org.guppy4j.datatree;
 /**
  * Tree structured data
  */
-public interface Tree extends WritablePayload, ReadableTree {
+public interface Tree extends WritableData, ReadableTree {
 
     Iterable<Tree> nodes(String expression);
 
