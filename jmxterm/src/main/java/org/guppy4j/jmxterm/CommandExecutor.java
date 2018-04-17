5d7a0a7b3fe1ad5c907a5c232b8187727f51f474
odoepner <oliver@doepner.net>
2018-04-17 13:45:19 -0300

    Convenience API for executing jmxterm commands programmatically

---
+++ jmxterm/src/main/java/org/guppy4j/jmxterm/CommandExecutor.java
@@ -0,0 +1,22 @@
+package org.guppy4j.jmxterm;
+
+/**
+ * Allows programmatic execution of <a href="http://wiki.cyclopsgroup.org/jmxterm/">jmxterm</a> commands
+ */
+public interface CommandExecutor extends AutoCloseable {
+
+	/**
+	 * Executes a jmxterm command. Use the 'help' command for a list of supported commands.
+	 *
+	 * @return the output of the successful command
+	 * @throws IllegalStateException containing any error messages, when an error occurs
+	 */
+	String execute(String command);
+
+	/**
+	 * Implementations won't throw any checked exceptions.
+	 */
+	@Override
+	void close();
+
+}
